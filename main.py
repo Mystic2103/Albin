1507c63906b7955dbcb07f8ed908b11ee5b45031
Mystic <mystic21003@gmail.com>
2021-06-04 00:15:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 bot = commands.Bot(command_prefix = "!", intents = discord.Intents.default())
-token = os.getenv('DISCORD_TOKEN')
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 
 @bot.event
@@ -19,6 +21,6 @@ async def ping(ctx):
 
 
 
-bot.run(token)
+bot.run(TOKEN)
 
 
