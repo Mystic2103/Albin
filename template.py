29be618bedfd37cf880ba289a3843c36c92427e6
Mystic <52890898+Mystic2103@users.noreply.github.com>
2021-06-03 21:03:36 +0200

    Update template.py

--- template.py
+++ template.py
@@ -6,7 +6,7 @@ bot = commands.Bot(command_prefix = "!", intents = discord.Intents.default())
 @bot.event
 async def on_ready():
     print("Online")
-    await bot.change_presence(activity = discord.Streaming(name = "your mom's sex tape", url = "https://www.youtube.com/watch?v=5qap5aO4i9A")) #status = discord.Status.(dnd)(online)(idle)etc...
+    await bot.change_presence(activity = discord.Streaming(name = "Lofi", url = "https://www.youtube.com/watch?v=5qap5aO4i9A")) #status = discord.Status.(dnd)(online)(idle)etc...
 
 
 @bot.command()
